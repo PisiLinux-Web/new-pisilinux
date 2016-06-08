338f2f8adcfd24334eaaa437c619b6822ed77542
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2016-06-09 01:39:57 +0300

    Index page responsive fixing

--- js/intro.js
+++ js/intro.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
-    checkScrool();
-});
-
-$(window).scroll(function() {
-    checkScrool();
+    if($(document).width() > 768) {
+        checkScrool();
+        $(window).scroll(function () {
+            checkScrool();
+        });
+    } else {
+        fixedNav();
+    }
 });
 
 function checkScrool(){
     if ( $(document).scrollTop() > 50) {
-        $("#topNav").addClass("navbar navbar-default navbar-header navbar-fixed-top").removeClass("static-nav");
+        fixedNav();
     } else {
-        $("#topNav").addClass("static-nav").removeClass("navbar navbar-default navbar-header navbar-fixed-top");
+        staticNav();
     }
+}
+
+function staticNav(){
+    $("#topNav").addClass("static-nav").removeClass("navbar navbar-default navbar-header navbar-fixed-top");
+}
+
+function fixedNav(){
+    $("#topNav").addClass("navbar navbar-default navbar-header navbar-fixed-top").removeClass("static-nav");
 }
